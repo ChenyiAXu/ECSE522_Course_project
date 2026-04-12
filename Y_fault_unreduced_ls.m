abce24f90083548e908fae6464a22401b61b5414
Chenyi Xu <115749117+ChenyiAXu@users.noreply.github.com>
2026-04-12 00:28:38 -0400

    Add comments for input parameters in Y_fault_unreduced_ls
    Added comments to clarify input parameters for the function.

--- Y_fault_unreduced_ls.m
+++ Y_fault_unreduced_ls.m
@@ -1,4 +1,6 @@
 function Y2_prim =  Y_fault_unreduced_ls(Y1_prim, fault_bus)
+%Y1_prime: prefault unreduced admittance matrix with load shunt
+%fault_bus: faulted bus index
 keep = setdiff(1:size(Y1_prim,1), fault_bus);
 Y2_prim = Y1_prim(keep, keep);
 end %end of function
