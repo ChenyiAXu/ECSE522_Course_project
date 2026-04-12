7299ac89a59a12c6a54e005386fa4c38e9a308aa
Chenyi Xu <115749117+ChenyiAXu@users.noreply.github.com>
2026-04-12 00:33:29 -0400

    Enhance comments in Y_prefault_unreduced_ls function
    Added comments to clarify input parameters and functionality.

--- Y_prefault_unreduced_ls.m
+++ Y_prefault_unreduced_ls.m
@@ -1,4 +1,10 @@
 function Y1_prim = Y_prefault_unreduced_ls(Y_prim_prim, load_index, P, Q, V_mag)
+% Y_prim_prim : unreduced prefault admittance matrix before adding load shunts
+% load_index  : bus indices where loads are connected
+% P           : bus active power vector
+% Q           : bus reactive power vector
+% V_mag       : bus voltage magnitude vector from power flow
+% Y1_prim     : prefault admittance matrix after adding load shunt admittances
 nodes = size(Y_prim_prim, 1);
 Y_ls = zeros(nodes, nodes);
 nloadBus = length(load_index);
@@ -10,8 +16,8 @@ Vmag_load = V_mag(load_index);
 S_conj = conj(P_load + 1i*Q_load);
 Y_load = S_conj ./ (Vmag_load .^2);
 for i = 1:nloadBus
-    Y_ls(load_index(i),load_index(i))= Y_load(i);
+    Y_ls(load_index(i),load_index(i))= Y_load(i); %add to the diagonal component
 end %end for loop
-
+%add the matrix to Y''
 Y1_prim = Y_prim_prim + Y_ls;
 end %end of function
