7c7d8bc5d40a39e979346af3d6df552c071c4220
Chenyi Xu <115749117+ChenyiAXu@users.noreply.github.com>
2026-04-12 00:35:05 -0400

    Add documentation for machineVoltage function
    Added function documentation for machineVoltage inputs and outputs.

--- machineVoltage.m
+++ machineVoltage.m
@@ -1,4 +1,12 @@
 function [E_mag, E_ang] = machineVoltage(P, Q, V_mag, V_ang, x_int, gen_index)
+% P         : bus active power vector from power flow
+% Q         : bus reactive power vector from power flow
+% V_mag     : bus voltage magnitude vector
+% V_ang     : bus voltage angle vector
+% x_int     : internal reactance of each generator
+% gen_index : bus indices where generators are connected
+% E_mag     : internal voltage magnitude of each generator
+% E_ang     : internal voltage angle of each generator
 Pg = P(gen_index);
 Qg = Q(gen_index);
 V = V_mag .* exp(1j * deg2rad(V_ang));
